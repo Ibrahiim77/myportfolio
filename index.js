2c47632f8a3321a7359655cf0c2a3826f7f223e5
Ibrahim <ibrahimjkxd@Gmail.com>
2025-11-21 01:27:20 +0500

    Portfolio

---
+++ index.js
@@ -0,0 +1,6 @@
+ document.querySelector(".a").onclick = () => { window.location.href = "index.html"; };
+  document.querySelector(".b").onclick = () => { window.location.href = "skills.html"; };
+  document.querySelector(".d").onclick = () => { window.location.href = "projects.html"; };
+  document.querySelector(".c").onclick = () => { window.location.href = "contacts.html"; };
+
+  document.getElementById("GoToprojects").onclick =() => {window.location.href= "projects.html";};
